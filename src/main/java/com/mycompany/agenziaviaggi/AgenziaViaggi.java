42f9c7ab6d4e89000c25dcebb7efd9e3599c4d34
Isaac <lorenzociani1002@gmail.com>
2024-12-13 12:21:53 +0100

    primo commit

---
+++ src/main/java/com/mycompany/agenziaviaggi/AgenziaViaggi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenziaviaggi;
+
+/**
+ *
+ * @author isaac
+ */
+public class AgenziaViaggi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
