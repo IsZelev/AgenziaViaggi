a1b6aa6e8ceac5c1ef1fb3a694016bec22115dd4
Isaac <lorenzociani1002@gmail.com>
2024-12-19 16:07:59 +0100

    nuove modifiche

--- AgenziaViaggi/src/main/java/com/mycompany/agenziaviaggi/AgenziaViaggi.java
+++ AgenziaViaggi/src/main/java/com/mycompany/agenziaviaggi/AgenziaViaggi.java
@@ -4,14 +4,32 @@
 
 package com.mycompany.agenziaviaggi;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author isaac
  */
-public class AgenziaViaggi {
+public class AgenziaViaggi
+{
+    private ArrayList<Camping> camps = new ArrayList<>();
 
-    public static void main(String[] args)
+    public void main(String[] args)
     {
-        System.out.println("");
+        System.out.println("Organizzazione dei viaggi organizzati");
+        CampFR camping_FR = new CampFR();
+        camps.add(camping_FR);
+        CampSP camping_SP = new CampSP();
+        camps.add(camping_SP);
+        CampDE camping_DE = new CampDE();
+        camps.add(camping_DE);
+        
+        for(Camping camp : camps)
+        {
+            String lang = camp.getLanguage();
+            Bus bus = new Bus(lang);
+            bus.run();
+        }
+        
     }
 }
